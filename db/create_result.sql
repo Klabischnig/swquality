28146f3ba98e09be84f6d47a9e983ad45e9bc85a
kerstin <kerstin.klabischnig@student.uibk.ac.at>
2020-10-11 16:05:20 +0200

    delete folder db

--- db/create_result.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `result` (
-  `result` enum('passed','failed','not executed') NOT NULL,
-  PRIMARY KEY (`result`)
-);
-
-insert into result (result) values
-("passed"), ("failed"),("not executed");
